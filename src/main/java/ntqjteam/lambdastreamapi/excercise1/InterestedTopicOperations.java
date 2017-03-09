c9cb866ca9ed8b1babf987a77989522a1cdb38e9
HUONGPC\HUONGPC <huong.nguyen.intern@ntq-solution.com.vn>
2017-03-09 11:23:30 +0700

    up bai tap lambda_java

--- src/main/java/ntqjteam/lambdastreamapi/excercise1/InterestedTopicOperations.java
+++ src/main/java/ntqjteam/lambdastreamapi/excercise1/InterestedTopicOperations.java
@@ -1,6 +1,8 @@
 package ntqjteam.lambdastreamapi.excercise1;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by toan on 3/1/17.
@@ -8,15 +10,42 @@ import java.util.List;
 public class InterestedTopicOperations {
 
     public static void main(String[] args) {
+    	Topic topic1=new Topic("1", "topic 1", "description 1");
+    	Topic topic2=new Topic("2", "topic 2", "description 2");
+    	Topic topic3=new Topic("3", "topic 3", "description 3");
+    	Topic topic4=new Topic("4", "topic 4", "description 4");
+    	Topic topic5=new Topic("5", "topic 5", "description 5");
+    	Topic topic6=new Topic("6", "topic 6", "description 6");
+    	Topic topic7=new Topic("7", "topic 7", "description 7");
+    	Topic topic8=new Topic("8", "topic 8", "description 8");
+    	Topic topic9=new Topic("9", "topic 9", "description 9");
+    	Topic topic10=new Topic("10", "topic 10", "description 10");
+    	Topic topic11=new Topic("11", "topic 11", "description 11");
+    	
 
-        List<Topic> currentTopics; // needs initializing
-        List<Topic> updatingTopics; // needs initializing
+        List<Topic> currentTopics=Arrays.asList(topic1,topic2,topic3,topic4,topic5,topic6,topic7,topic8); // needs initializing
+        System.out.println("Current topics");
+        printlnListTopic(currentTopics);
+        List<Topic> updatingTopics=Arrays.asList(topic4,topic5,topic6,topic7,topic8,topic9,topic10,topic11); // needs initializing
+        System.out.println("updating topics");
+        printlnListTopic(updatingTopics);
 
         // Find out these groups of topics from currentTopics and updatingTopics
-        List<Topic> addedTopics;
-        List<Topic> updatedTopics;
-        List<Topic> deletedTopics;
+        List<Topic> addedTopics=updatingTopics.stream().filter(topic->currentTopics.contains(topic)==false).collect(Collectors.toList());
+        System.out.println("added topics");
+        printlnListTopic(addedTopics);
+        List<Topic> updatedTopics=updatingTopics.stream().filter(topic->currentTopics.contains(topic)==true).collect(Collectors.toList());
+        System.out.println("updated topics");
+        printlnListTopic(updatedTopics);
+        List<Topic> deletedTopics=currentTopics.stream().filter(topic->updatingTopics.contains(topic)==false).collect(Collectors.toList());
+        System.out.println("deleted topics");
+        printlnListTopic(deletedTopics);
 
     }
-
+private static void printlnListTopic(List<Topic> listTopic){
+	for (Topic topic : listTopic) {
+		String s="topic: "+topic.getId()+" \t name: "+topic.getName()+"\t description: "+topic.getDescription();
+		System.out.println(s);
+	}
+}
 }
--- src/main/java/ntqjteam/lambdastreamapi/excercise2/ProgrammerCategorizing.java
+++ src/main/java/ntqjteam/lambdastreamapi/excercise2/ProgrammerCategorizing.java
@@ -1,20 +1,40 @@
 package ntqjteam.lambdastreamapi.excercise2;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import ntqjteam.lambdastreamapi.excercise2.Programmer.Gender;
 
 /**
  * Created by toan on 3/1/17.
  */
 public class ProgrammerCategorizing {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		Programmer programmer1 = new Programmer("id1", "name1", "division1", "team1", Gender.FEMALE);
+		Programmer programmer2 = new Programmer("id2", "name2", "division1", "team1", Gender.MALE);
+		Programmer programmer3 = new Programmer("id3", "name3", "division1", "team2", Gender.FEMALE);
+		Programmer programmer4 = new Programmer("id4", "name4", "division1", "team2", Gender.FEMALE);
+		Programmer programmer5 = new Programmer("id5", "name5", "division2", "team1", Gender.MALE);
+		Programmer programmer6 = new Programmer("id6", "name6", "division2", "team1", Gender.FEMALE);
+		Programmer programmer7 = new Programmer("id7", "name7", "division2", "team2", Gender.MALE);
+		Programmer programmer8 = new Programmer("id8", "name8", "division2", "team2", Gender.MALE);
+		Programmer programmer9 = new Programmer("id9", "name9", "division3", "team1", Gender.FEMALE);
+		Programmer programmer10 = new Programmer("id10", "name10", "division3", "team2", Gender.FEMALE);
+		Programmer programmer11 = new Programmer("id11", "name11", "division3", "team2", Gender.MALE);
 
-        List<Programmer> programmers;  // init
+		List<Programmer> programmers = Arrays.asList(programmer1, programmer2, programmer3, programmer4, programmer5,
+				programmer6, programmer7, programmer8, programmer9, programmer10, programmer11); // init
 
-        // Categorize programmers into divisions
+		// Categorize programmers into divisions
+		Map<String, List<Programmer>> listProgrammerGroupbyDivisions= programmers.stream().collect(Collectors.groupingBy(Programmer::getDivision));
+		System.out.println(listProgrammerGroupbyDivisions);
+		// Categorize programmers into divisions, then teams, then genders
+		Map<String, Map<String, Map<Programmer.Gender, List<Programmer>>>> listProgrammerGruopbyDivisionTeamGender=programmers.stream().collect(Collectors.groupingBy(Programmer::getDivision,Collectors.groupingBy(Programmer::getTeam,Collectors.groupingBy(Programmer::getGender))));
+		System.out.println(listProgrammerGruopbyDivisionTeamGender);
 
-        // Categorize programmers into divisions, then teams, then genders
-
-    }
+	}
 
 }
